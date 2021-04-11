e614a0c07cca3f4d972e78f746dc1b0e42b61610
frandy-yulia <frandy.yulia@gmail.com>
2021-04-11 22:11:19 +0700

    set default valu modal box

--- src/browser/components/modalbox/index.js
+++ src/browser/components/modalbox/index.js
@@ -5,25 +5,45 @@ import './styles.css';
 class ModalBox extends Component {
     constructor(props) {
         super(props);
-        this.state = {selected: null,message: null}
+        this.state = {
+            selected: null,
+            message: null,
+            value: null
+        }
     }
-
 
     componentDidMount() {
         this.setState({selected: this.props.onSelected()});
+        switch(this.props.message){
+            case 'Open Folder': 
+                this.setState({value: process.env.HOME})
+            break;
+        }
+    }
+
+    hendleClick() {
+        console.log(this.state.value);
+    }
+
+    hendleChange(e) {
+        this.setState({value: e.target.value});
     }
 
     render() {
         return (
-            <div className="ModalBox" {...this.props}>
+            <div className="ModalBox">
                 <div className="container">
                     <div className="top">
                         <div className="label">{this.props.message}</div>
                         <div className="row">
-                            <input type="text"/>
-                            <button type="button" className="clickable">ok</button>
+                            <input type="text" value={this.state.value} onChange={this.hendleChange.bind(this)}/>
+                            <button type="button" className="clickable" onClick={this.hendleClick.bind(this)}>
+                                ok
+                            </button>
                         </div>
                     </div>
+
+                    <div {...this.props} className="close-modal"></div>
                 </div>
             </div>
         );
